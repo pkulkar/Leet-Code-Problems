1619e0e9f6f60b4e98adf941ec6d67f22bd1363d
pkulkar <36709057+pkulkar@users.noreply.github.com>
2019-04-19 19:34:07 -0400

    Update strstr.py

--- Python Solutions/strstr.py
+++ Python Solutions/strstr.py
@@ -2,6 +2,8 @@ class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         if needle=="":
             return 0
+        if needle not in haystack:
+            return -1
         else:
             for i in range(0,len(haystack)-len(needle)+1,1):
                 sub=""
@@ -9,5 +11,5 @@ class Solution:
                     sub+=haystack[i+j]                    
                 if sub==needle:
                     return(i)
-            return -1
+           
         
